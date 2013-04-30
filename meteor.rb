f749be827f3cf69d7407b4fa2ef17d2fafed96c7
hikaruworld <hikaruworld@gmail.com>
2013-05-01 01:00:29 +0900

    version up 0.6.2.1

--- meteor.rb
+++ meteor.rb
@@ -3,9 +3,9 @@ require 'formula'
 
 class Meteor < Formula
   homepage 'http://www.meteor.com'
-  url 'https://d3fm2vapipm3k9.cloudfront.net/bootstrap/0.6.2/meteor-bootstrap-Darwin_x86_64.tar.gz'
-  sha1 'a8840ab8e35bb0a0fe9f546b66d520b11588049e'
-  version '0.6.2'
+  url 'https://d3fm2vapipm3k9.cloudfront.net/bootstrap/0.6.2.1/meteor-bootstrap-darwin_x86_64.tar.gz'
+  sha1 '8e8f0b33e5e874723aa4f862e7d8a1866d9d456a'
+  version '0.6.2.1'
 
   def install
     libexec.install(Dir['{.*,*}'] - ['.', '..']) # includes dotfiles
