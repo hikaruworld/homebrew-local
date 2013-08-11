db7393cd4aac77e44558f5d3bf1a00301c89aa85
hikaruworld <hikaruworld@gmail.com>
2013-08-11 23:21:51 +0900

    また間違っていたので修正

--- bashmarks.rb
+++ bashmarks.rb
@@ -4,7 +4,7 @@ class Bashmarks < ScriptFileFormula
   head 'https://github.com/hikaruworld/bashmarks.git', :revision => 'cc0bd41c3bdd5d0cda77a9d736e1db1523104f9d'
 
   def install
-    bin.install Dir['bin/*']
+    bin.install Dir['bin/bashmarks']
   end
 
 end
