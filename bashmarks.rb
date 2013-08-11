df02bc59f99c7bab7911a40358ba1d4cc27fdd12
hikaruworld <hikaruworld@gmail.com>
2013-08-11 15:48:06 +0900

    # This is a combination of 4 commits. # The first commit's message is: bashmarks.rbを追加
    # The 2nd commit message will be skipped:
    
    #	bashmarksをheadに更新
    
    # The 3rd commit message will be skipped:
    
    #	コマンドミスを修正
    
    # The 4th commit message will be skipped:
    
    #	一旦コメントアウト

---
+++ bashmarks.rb
@@ -0,0 +1,11 @@
+require 'formula'
+
+class Bashmarks < Formula
+  head 'https://github.com/huyng/bashmarks.git', :revision => 'f3911e3a0f1fb72daa384a2b56960529b64c0602'
+  homepage 'http://www.huyng.com/projects/bashmarks/'
+
+  def install
+    #system ""
+  end
+
+end
